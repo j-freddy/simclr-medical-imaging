c7b8b7212d4b756ddfda71ba696397e62ae51ce4
F Jiang <jiang.tf.2020@gmail.com>
2023-01-30 11:45:11 +0000

    Small refactor

--- simclr/train.py
+++ simclr/train.py
@@ -5,5 +5,5 @@ from utils import setup_device
 if __name__=="__main__":
   pl.seed_everything(SEED)
   device = setup_device()
-  print("Device:", device)
-  print("Number of workers:", NUM_WORKERS)
+  print(f"Device: {device}")
+  print(f"Number of workers: {NUM_WORKERS}")
