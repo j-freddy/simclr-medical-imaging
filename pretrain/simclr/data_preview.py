3747ada4ee745ea65515bdda7c3f8c0559826b12
Freddy Jiang <jtf.imperial@gmail.com>
2023-04-25 12:29:42 +0100

    Optimise imports for data preview

--- pretrain/simclr/data_preview.py
+++ pretrain/simclr/data_preview.py
@@ -1,26 +1,13 @@
-import os
-import sys
 import pytorch_lightning as pl
-from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
-from pytorch_lightning.loggers import TensorBoardLogger
-import torch.utils.data as data
 from downloader import Downloader
 
 from pretrain.simclr.contrastive_downloader import ContrastiveDownloader
-from pretrain.simclr.simclrlm import SimCLRLM
-from pretrain.simclr.utils import (
-    get_pretrained_model,
-    summarise,
-)
 from utils import (
     NUM_WORKERS,
     SEED,
-    SIMCLR_CHECKPOINT_PATH,
     SplitType,
-    get_accelerator_info,
     parse_args_img_viewer,
     setup_device,
-    show_example_images,
     show_original_and_augmented_example_images,
 )
 
